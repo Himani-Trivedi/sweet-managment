e3f9b3edfe9a6cdbba9e5eb8a0cb5ccc1f7aeb2d
Himani-Trivedi <himanitrivedi1874@gmail.com>
2025-12-13 12:29:52 +0530

    chore: initial project setup

---
+++ src/main/java/com/api/mithai/SweetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.api.mithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/mithai/SweetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mithai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
